03c9508a05f1f33f7e777c45813ba05875dd8fc4
roynaldi19 <roynaldi19@gmail.com>
2023-02-28 19:26:43 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/roynaldi_test2/model/JakartaResponse.kt
@@ -0,0 +1,165 @@
+package com.roynaldi19.roynaldi_test2.model
+
+import com.google.gson.annotations.SerializedName
+
+data class JakartaResponse(
+
+	@field:SerializedName("city")
+	val city: City,
+
+	@field:SerializedName("cnt")
+	val cnt: Int,
+
+	@field:SerializedName("cod")
+	val cod: String,
+
+	@field:SerializedName("message")
+	val message: Int,
+
+	@field:SerializedName("list")
+	val list: List<ListItem>
+)
+
+data class Rain(
+
+	@field:SerializedName("3h")
+	val jsonMember3h: Any
+)
+
+data class Coord(
+
+	@field:SerializedName("lon")
+	val lon: Any,
+
+	@field:SerializedName("lat")
+	val lat: Any
+)
+
+data class Main(
+
+	@field:SerializedName("temp")
+	val temp: Any,
+
+	@field:SerializedName("temp_min")
+	val tempMin: Any,
+
+	@field:SerializedName("grnd_level")
+	val grndLevel: Int,
+
+	@field:SerializedName("temp_kf")
+	val tempKf: Any,
+
+	@field:SerializedName("humidity")
+	val humidity: Int,
+
+	@field:SerializedName("pressure")
+	val pressure: Int,
+
+	@field:SerializedName("sea_level")
+	val seaLevel: Int,
+
+	@field:SerializedName("feels_like")
+	val feelsLike: Any,
+
+	@field:SerializedName("temp_max")
+	val tempMax: Any
+)
+
+data class Sys(
+
+	@field:SerializedName("pod")
+	val pod: String
+)
+
+data class Clouds(
+
+	@field:SerializedName("all")
+	val all: Int
+)
+
+data class Wind(
+
+	@field:SerializedName("deg")
+	val deg: Int,
+
+	@field:SerializedName("speed")
+	val speed: Any,
+
+	@field:SerializedName("gust")
+	val gust: Any
+)
+
+data class City(
+
+	@field:SerializedName("country")
+	val country: String,
+
+	@field:SerializedName("coord")
+	val coord: Coord,
+
+	@field:SerializedName("sunrise")
+	val sunrise: Int,
+
+	@field:SerializedName("timezone")
+	val timezone: Int,
+
+	@field:SerializedName("sunset")
+	val sunset: Int,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("population")
+	val population: Int
+)
+
+data class ListItem(
+
+	@field:SerializedName("dt")
+	val dt: Int,
+
+	@field:SerializedName("pop")
+	val pop: Any,
+
+	@field:SerializedName("rain")
+	val rain: Rain,
+
+	@field:SerializedName("visibility")
+	val visibility: Int,
+
+	@field:SerializedName("dt_txt")
+	val dtTxt: String,
+
+	@field:SerializedName("weather")
+	val weather: List<WeatherItem>,
+
+	@field:SerializedName("main")
+	val main: Main,
+
+	@field:SerializedName("clouds")
+	val clouds: Clouds,
+
+	@field:SerializedName("sys")
+	val sys: Sys,
+
+	@field:SerializedName("wind")
+	val wind: Wind
+)
+
+data class WeatherItem(
+
+	@field:SerializedName("icon")
+	val icon: String,
+
+	@field:SerializedName("description")
+	val description: String,
+
+	@field:SerializedName("main")
+	val main: String,
+
+	@field:SerializedName("id")
+	val id: Int
+)
